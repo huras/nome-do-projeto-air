d6979288821b6c85fff9b000d6a746799d01a796
huras alexandre <hurast@gmail.com>
2025-04-17 05:06:32 -0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +19,29 @@ Route::get('/sobre-nos', function () {
 Route::get('/nossos-parceiros', function () {
     return view('pages.nossos-parceiros');
 })->name('nossos-parceiros');
+
+Route::get('/cases-area-externa', function () {
+    return view('pages.cases-area-externa');
+})->name('cases-externa');
+
+Route::get('/cases-area-interna', function () {
+    return view('pages.case.cases-area-interna');
+})->name('cases-interna');
+
+Route::get('/sou-creator', function () {
+    return view('pages.sou-creator');
+})->name('sou-creator');
+
+Route::get('/contato', function () {
+    return view('pages.contato');
+})->name('contato');
+
+Route::get('/my-air', function () {
+    return view('pages.my-air');
+})->name('my-air');
+
+Route::get('/privacy-policy', function () {
+    return view('pages.privacy-policy');
+})->name('privacy-policy');
+
+// Route::post('/contact/send', [ContactController::class, 'send'])->name('contact.send');
